26b6190dc47916d6f87f2acb9def3646baac0cc0
Unstoppable <VipendraSinghPatel999@gmail.com>
2024-10-22 10:36:05 +0530

    Add Phase-01-Part-B

---
+++ Phase-01-Part-B/models/listing.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    default:
+      "https://plus.unsplash.com/premium_photo-1661915661139-5b6a4e4a6fcc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG91c2V8ZW58MHx8MHx8fDA%3D",
+    set: (v) =>
+      v === ""
+        ? "https://plus.unsplash.com/premium_photo-1661915661139-5b6a4e4a6fcc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG91c2V8ZW58MHx8MHx8fDA%3D"
+        : v,
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;
+
+// Keep in mind -
+// As soon as you create the model, the database gets created.
+// You just need to export from here & import in app.js. That's it...
+
+// The name of the collection will automatically be in Plural Form
+// i.e. Listing --> listings
