cde48d4fedeed3513520d05ebe9ad3fb47d6e5e9
Unstoppable <VipendraSinghPatel999@gmail.com>
2024-10-25 10:37:26 +0530

    Add Phase-01-Part-C

---
+++ Phase-01-Part-C/schema.js
@@ -0,0 +1,22 @@
+const Joi = require("joi");
+// module.exports = Joi.object({
+//   listing: Joi.object({
+//     title: Joi.string().required(),
+//     description: Joi.string().required(),
+//     location: Joi.string().required(),
+//     country: Joi.string().required(),
+//     price: Joi.number().required().min(0),
+//     image: Joi.string().allow("", null),
+//   }).required(),
+// });
+
+module.exports.listingSchema = Joi.object({
+  listing: Joi.object({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    location: Joi.string().required(),
+    country: Joi.string().required(),
+    price: Joi.number().required().min(0),
+    image: Joi.string().allow("", null),
+  }).required(),
+});
